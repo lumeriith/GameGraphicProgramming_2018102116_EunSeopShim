039f21f74f8d1246526881acf8dc203a1d923031
Eunseop Shim <lumeriith@gmail.com>
2022-03-20 03:19:36 +0900

    [LAB01] Apply starter pack for lab01

--- Source/Library/Common.h
+++ Source/Library/Common.h
@@ -1,10 +1,12 @@
-﻿#pragma once
 /*+===================================================================
   File:      COMMON.H
-  Summary:   Common header file that contains common header files and
+
+  Summary:   Common header file that contains common header files and 
 			 macros used for the Library project of Game Graphics
 			 Programming course.
+
   Functions:
+
   © 2022 Kyung Hee University
 ===================================================================+*/
 #pragma once
@@ -14,3 +16,12 @@
 #endif // ! UNICODE
 
 #include <windows.h>
+
+#include <d3d11_4.h>
+#include <directxcolors.h>
+
+#include "Resource.h"
+
+constexpr LPCWSTR PSZ_COURSE_TITLE = L"Game Graphics Programming";
+
+using namespace DirectX;
--- Source/Library/Game/Game.cpp
+++ Source/Library/Game/Game.cpp
@@ -2,12 +2,38 @@
 
 namespace library
 {
-    /*--------------------------------------------------------------------
-      Function definitions
-    --------------------------------------------------------------------*/
-    void PrintHi()
-    {
-        OutputDebugString(L"hi\n");
-        MessageBox(nullptr, L"hi", L"Game Graphics Programming", MB_OK);
-    }
+	/*--------------------------------------------------------------------
+	  Global Variables
+	--------------------------------------------------------------------*/
+	/*--------------------------------------------------------------------
+	  TODO: Initialize global variables (remove the comment)
+	--------------------------------------------------------------------*/
+
+	/*--------------------------------------------------------------------
+	  Forward declarations
+	--------------------------------------------------------------------*/
+
+	/*F+F+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+	  Function: WindowProc
+
+	  Summary:  Defines the behavior of the window—its appearance, how
+				it interacts with the user, and so forth
+
+	  Args:     HWND hWnd
+				  Handle to the window
+				UINT uMsg
+				  Message code
+				WPARAM wParam
+				  Additional data that pertains to the message
+				LPARAM lParam
+				  Additional data that pertains to the message
+
+	  Returns:  LRESULT
+				  Integer value that your program returns to Windows
+	-----------------------------------------------------------------F-F*/
+	LRESULT CALLBACK WindowProc(_In_ HWND hWnd, _In_ UINT uMsg, _In_ WPARAM wParam, _In_ LPARAM lParam);
+
+	/*--------------------------------------------------------------------
+	  TODO: Function definitions (remove this comment)
+	--------------------------------------------------------------------*/
 }
