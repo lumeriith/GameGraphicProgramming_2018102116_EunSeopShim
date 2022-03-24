51882fdaf1f109ad71cd08a769a46901b0b27a68
Eunseop Shim <lumeriith@gmail.com>
2022-03-25 06:35:31 +0900

    [LAB02] Fix misconfigured file paths

--- Source/Library/Renderer/Renderer.cpp
+++ Source/Library/Renderer/Renderer.cpp
@@ -1,45 +1,210 @@
 #include "Renderer/Renderer.h"
 
-namespace library
-{
-    /*M+M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M
-      Method:   Renderer::Renderer
+namespace library {
+	const D3D_FEATURE_LEVEL FEATURE_LEVELS[] = {
+	D3D_FEATURE_LEVEL_9_1,
+	D3D_FEATURE_LEVEL_9_2,
+	D3D_FEATURE_LEVEL_9_3,
+	D3D_FEATURE_LEVEL_10_0,
+	D3D_FEATURE_LEVEL_10_1,
+	D3D_FEATURE_LEVEL_11_0,
+	D3D_FEATURE_LEVEL_11_1
+	};
 
-      Summary:  Constructor
+	/*M+M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M
+	  Method:   Renderer::Renderer
 
-      Modifies: [m_driverType, m_featureLevel, m_d3dDevice, m_d3dDevice1, 
-                  m_immediateContext, m_immediateContext1, m_swapChain, 
-                  m_swapChain1, m_renderTargetView].
-    M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M-M*/
-    /*--------------------------------------------------------------------
-      TODO: Renderer::Renderer definition (remove the comment)
-    --------------------------------------------------------------------*/
+	  Summary:  Constructor
 
-    /*M+M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M
-      Method:   Renderer::Initialize
+	  Modifies: [m_driverType, m_featureLevel, m_d3dDevice, m_d3dDevice1,
+				  m_immediateContext, m_immediateContext1, m_swapChain,
+				  m_swapChain1, m_renderTargetView].
+	M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M-M*/
+	Renderer::Renderer()
+	{
+		m_driverType = D3D_DRIVER_TYPE_HARDWARE;
+		m_featureLevel = D3D_FEATURE_LEVEL_11_1;
 
-      Summary:  Creates Direct3D device and swap chain
+		m_d3dDevice = nullptr;
+		m_d3dDevice1 = nullptr;
+		m_immediateContext = nullptr;
+		m_immediateContext1 = nullptr;
+		m_swapChain = nullptr;
+		m_swapChain1 = nullptr;
+		m_renderTargetView = nullptr;
+	}
 
-      Args:     HWND hWnd
-                  Handle to the window
+	/*M+M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M
+	  Method:   Renderer::Initialize
 
-      Modifies: [m_d3dDevice, m_featureLevel, m_immediateContext, 
-                  m_d3dDevice1, m_immediateContext1, m_swapChain1, 
-                  m_swapChain, m_renderTargetView].
+	  Summary:  Creates Direct3D device and swap chain
 
-      Returns:  HRESULT
-                  Status code
-    M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M-M*/
-    /*--------------------------------------------------------------------
-      TODO: Renderer::Initialize definition (remove the comment)
-    --------------------------------------------------------------------*/
+	  Args:     HWND hWnd
+				  Handle to the window
 
-    /*M+M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M
-      Method:   Renderer::Render
+	  Modifies: [m_d3dDevice, m_featureLevel, m_immediateContext,
+				  m_d3dDevice1, m_immediateContext1, m_swapChain1,
+				  m_swapChain, m_renderTargetView].
 
-      Summary:  Render the frame
-    M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M-M*/
-    /*--------------------------------------------------------------------
-      TODO: Renderer::Initialize definition (remove the comment)
-    --------------------------------------------------------------------*/
+	  Returns:  HRESULT
+				  Status code
+	M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M-M*/
+	HRESULT Renderer::Initialize(_In_ HWND hWnd)
+	{
+		HRESULT hr;
+
+		// ---------------------------------------------------------------------------------------------
+		// GetInterfaceForDeviceAndContext 
+		// ---------------------------------------------------------------------------------------------
+
+		// This flag adds support for surfaces with a color-channel ordering different
+		// from the API default. It is required for compatibility with Direct2D.
+		UINT deviceFlags = D3D11_CREATE_DEVICE_BGRA_SUPPORT;
+
+#if defined(DEBUG) || defined(_DEBUG)
+		deviceFlags |= D3D11_CREATE_DEVICE_DEBUG;
+#endif
+		hr = D3D11CreateDevice(
+			nullptr,								// Specify nullptr to use the default adapter.
+			D3D_DRIVER_TYPE_HARDWARE,				// Create a device using the hardware graphics driver.
+			nullptr,								// Should be 0 unless the driver is D3D_DRIVER_TYPE_SOFTWARE.
+			deviceFlags,							// Set debug and Direct2D compatibility flags.
+			FEATURE_LEVELS,							// List of feature levels this app can support.
+			ARRAYSIZE(FEATURE_LEVELS),				// Size of the list above.
+			D3D11_SDK_VERSION,						// Always set this to D3D11_SDK_VERSION for Windows Store apps.
+			m_d3dDevice.GetAddressOf(),		// Returns the Direct3D device created.
+			&m_featureLevel,					// Returns feature level of device created.
+			m_immediateContext.GetAddressOf() // Returns the device immediate context.
+		);
+		if (FAILED(hr)) return hr;
+
+		m_d3dDevice.As(&m_d3dDevice1);
+		m_immediateContext.As(&m_immediateContext1);
+
+		// ---------------------------------------------------------------------------------------------
+		// CreateSwapChain 
+		// ---------------------------------------------------------------------------------------------
+
+		DXGI_SWAP_CHAIN_DESC desc = {};
+
+		desc.Windowed = TRUE; // Sets the initial state of full-screen mode.
+		desc.BufferCount = 2;
+		desc.BufferDesc.Format = DXGI_FORMAT_B8G8R8A8_UNORM;
+		desc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+		desc.SampleDesc.Count = 1;      //multisampling setting
+		desc.SampleDesc.Quality = 0;    //vendor-specific flag
+		desc.SwapEffect = DXGI_SWAP_EFFECT_FLIP_SEQUENTIAL;
+		desc.OutputWindow = hWnd;
+
+		// Create the DXGI device object to use in other factories, such as Direct2D.
+		ComPtr<IDXGIDevice3> pDxgiDevice;
+		m_d3dDevice.As(&pDxgiDevice);
+
+		// Create swap chain.
+		ComPtr<IDXGIAdapter> pAdapter;
+		ComPtr<IDXGIFactory> pFactory;
+
+		hr = pDxgiDevice->GetAdapter(&pAdapter);
+		if (FAILED(hr)) return hr;
+
+		hr = pAdapter->GetParent(IID_PPV_ARGS(&pFactory));
+		if (FAILED(hr)) return hr;
+
+		hr = pFactory->CreateSwapChain(
+			m_d3dDevice.Get(),
+			&desc,
+			m_swapChain.GetAddressOf()
+		);
+		if (FAILED(hr)) return hr;
+
+		m_swapChain.As(&m_swapChain1);
+
+		// ---------------------------------------------------------------------------------------------
+		// CreateRenderTarget 
+		// ---------------------------------------------------------------------------------------------
+
+		ComPtr<ID3D11Texture2D> pBackBuffer;
+		D3D11_TEXTURE2D_DESC bbDesc;
+
+		hr = m_swapChain->GetBuffer(
+			0,
+			__uuidof(ID3D11Texture2D),
+			(void**)pBackBuffer.GetAddressOf());
+		if (FAILED(hr)) return hr;
+
+		hr = m_d3dDevice->CreateRenderTargetView(
+			pBackBuffer.Get(),
+			nullptr,
+			m_renderTargetView.GetAddressOf()
+		);
+		if (FAILED(hr)) return hr;
+
+		pBackBuffer->GetDesc(&bbDesc);
+
+		// Create a depth-stencil buffer
+		ComPtr<ID3D11Texture2D> pDepthStencil;
+		ComPtr<ID3D11DepthStencilView> pDepthStencilView;
+
+		CD3D11_TEXTURE2D_DESC depthStencilDesc(
+			DXGI_FORMAT_D24_UNORM_S8_UINT,
+			bbDesc.Width,
+			bbDesc.Height,
+			1, // This depth stencil view has only one texture.
+			1, // Use a single mipmap level.
+			D3D11_BIND_DEPTH_STENCIL
+		);
+
+		hr = m_d3dDevice->CreateTexture2D(
+			&depthStencilDesc,
+			nullptr,
+			pDepthStencil.GetAddressOf()
+		);
+		if (FAILED(hr)) return hr;
+
+		CD3D11_DEPTH_STENCIL_VIEW_DESC depthStencilViewDesc(D3D11_DSV_DIMENSION_TEXTURE2D);
+
+		hr = m_d3dDevice->CreateDepthStencilView(
+			pDepthStencil.Get(),
+			&depthStencilViewDesc,
+			pDepthStencilView.GetAddressOf()
+		);
+		if (FAILED(hr)) return hr;
+
+		// Create a viewport
+		D3D11_VIEWPORT viewport = {};
+		viewport.Height = (float)bbDesc.Height;
+		viewport.Width = (float)bbDesc.Width;
+		viewport.MinDepth = 0;
+		viewport.MaxDepth = 1;
+
+		m_immediateContext->RSSetViewports(
+			1,
+			&viewport
+		);
+
+		return S_OK;
+	}
+
+
+	/*M+M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M
+	  Method:   Renderer::Render
+
+	  Summary:  Render the frame
+	M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M-M*/
+	void Renderer::Render()
+	{
+		// Clear the backbuffer
+		const float ClearColor[4] = { 0.0f, 0.125f, 0.6f, 1.0f }; // RGBA
+		m_immediateContext->ClearRenderTargetView(m_renderTargetView.Get(), ClearColor);
+
+		// Present
+		m_swapChain->Present(0, 0);
+	}
 }
+
+
+
+
+
+
+
--- Source/Library/Window/MainWindow.cpp
+++ Source/Library/Window/MainWindow.cpp
@@ -1,55 +1,83 @@
 #include "Window/MainWindow.h"
 
-namespace library
-{
-    /*M+M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M
-      Method:   MainWindow::Initialize
+namespace library {
+	/*M+M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M
+	  Method:   MainWindow::Initialize
 
-      Summary:  Initializes main window
+	  Summary:  Initializes main window
 
-      Args:     HINSTANCE hInstance
-                  Handle to the instance
-                INT nCmdShow
-                    Is a flag that says whether the main application window
-                    will be minimized, maximized, or shown normally
-                PCWSTR pszWindowName
-                    The window name
+	  Args:     HINSTANCE hInstance
+				  Handle to the instance
+				INT nCmdShow
+					Is a flag that says whether the main application window
+					will be minimized, maximized, or shown normally
+				PCWSTR pszWindowName
+					The window name
 
-      Returns:  HRESULT
-                  Status code
-    M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M-M*/
-    /*--------------------------------------------------------------------
-      TODO: MainWindow::Initialize definition (remove the comment)
-    --------------------------------------------------------------------*/
+	  Returns:  HRESULT
+				  Status code
+	M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M-M*/
+	HRESULT MainWindow::Initialize(_In_ HINSTANCE hInstance, _In_ INT nCmdShow, _In_ PCWSTR pszWindowName) {
+		return initialize(hInstance, nCmdShow, pszWindowName, WS_OVERLAPPEDWINDOW);
+	}
 
-    /*M+M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M
-      Method:   MainWindow::GetWindowClassName
 
-      Summary:  Returns the name of the window class
+	/*M+M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M
+	  Method:   MainWindow::GetWindowClassName
 
-      Returns:  PCWSTR
-                  Name of the window class
-    M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M-M*/
-    /*--------------------------------------------------------------------
-      TODO: MainWindow::GetWindowClassName definition (remove the comment)
-    --------------------------------------------------------------------*/
+	  Summary:  Returns the name of the window class
 
-    /*M+M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M
-      Method:   MainWindow::HandleMessage
+	  Returns:  PCWSTR
+				  Name of the window class
+	M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M-M*/
+	PCWSTR MainWindow::GetWindowClassName() const
+	{
+		return L"MainWindow";
+	}
 
-      Summary:  Handles the messages
 
-      Args:     UINT uMessage
-                  Message code
-                WPARAM wParam
-                    Additional data the pertains to the message
-                LPARAM lParam
-                    Additional data the pertains to the message
+	/*M+M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M
+	  Method:   MainWindow::HandleMessage
 
-      Returns:  LRESULT
-                  Integer value that your program returns to Windows
-    M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M-M*/
-    /*--------------------------------------------------------------------
-      TODO: MainWindow::HandleMessage definition (remove the comment)
-    --------------------------------------------------------------------*/
+	  Summary:  Handles the messages
+
+	  Args:     UINT uMessage
+				  Message code
+				WPARAM wParam
+					Additional data the pertains to the message
+				LPARAM lParam
+					Additional data the pertains to the message
+
+	  Returns:  LRESULT
+				  Integer value that your program returns to Windows
+	M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M-M*/
+	LRESULT MainWindow::HandleMessage(_In_ UINT uMsg, _In_ WPARAM wParam, _In_ LPARAM lParam)
+	{
+		switch (uMsg)
+		{
+		case WM_CLOSE:
+		{
+			HMENU hMenu;
+			hMenu = GetMenu(m_hWnd);
+			if (hMenu != nullptr)
+			{
+				DestroyMenu(hMenu);
+			}
+			DestroyWindow(m_hWnd);
+			UnregisterClass(
+				GetWindowClassName(),
+				m_hInstance
+			);
+			return 0;
+		}
+
+		case WM_DESTROY:
+			PostQuitMessage(0);
+			break;
+		default:
+			break;
+		}
+		return DefWindowProc(m_hWnd, uMsg, wParam, lParam);
+	}
 }
+
