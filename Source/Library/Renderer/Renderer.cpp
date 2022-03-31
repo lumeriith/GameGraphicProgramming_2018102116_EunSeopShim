64a762bf8358d4b4bac1d6b5ece32237428fa174
Eunseop Shim <lumeriith@gmail.com>
2022-03-31 22:27:31 +0900

    [LAB03] Apply starter pack

--- Source/Library/Renderer/Renderer.cpp
+++ Source/Library/Renderer/Renderer.cpp
@@ -8,7 +8,8 @@ namespace library {
 
 	  Modifies: [m_driverType, m_featureLevel, m_d3dDevice, m_d3dDevice1,
 				  m_immediateContext, m_immediateContext1, m_swapChain,
-				  m_swapChain1, m_renderTargetView].
+				  m_swapChain1, m_renderTargetView, m_vertexShader,
+				  m_pixelShader, m_vertexLayout, m_vertexBuffer].
 	M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M-M*/
 	Renderer::Renderer() :
 		m_driverType(D3D_DRIVER_TYPE_NULL),
@@ -32,7 +33,8 @@ namespace library {
 
 	  Modifies: [m_d3dDevice, m_featureLevel, m_immediateContext,
 				  m_d3dDevice1, m_immediateContext1, m_swapChain1,
-				  m_swapChain, m_renderTargetView].
+				  m_swapChain, m_renderTargetView, m_vertexShader,
+				  m_vertexLayout, m_pixelShader, m_vertexBuffer].
 
 	  Returns:  HRESULT
 				  Status code
@@ -201,6 +203,35 @@ namespace library {
 
 		// Present
 		m_swapChain->Present(0, 0);
+	}
+
+	/*M+M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M
+	  Method:   Renderer::compileShaderFromFile
+
+	  Summary:  Helper for compiling shaders with D3DCompile
+
+	  Args:     PCWSTR pszFileName
+				  A pointer to a constant null-terminated string that
+				  contains the name of the file that contains the
+				  shader code
+				PCSTR pszEntryPoint
+				  A pointer to a constant null-terminated string that
+				  contains the name of the shader entry point function
+				  where shader execution begins
+				PCSTR pszShaderModel
+				  A pointer to a constant null-terminated string that
+				  specifies the shader target or set of shader
+				  features to compile against
+				ID3DBlob** ppBlobOut
+				  A pointer to a variable that receives a pointer to
+				  the ID3DBlob interface that you can use to access
+				  the compiled code
+
+	  Returns:  HRESULT
+				  Status code
+	M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M-M*/
+	HRESULT Renderer::compileShaderFromFile(_In_ PCWSTR pszFileName, _In_ PCSTR pszEntryPoint, _In_ PCSTR szShaderModel, _Outptr_ ID3DBlob** ppBlobOut) {
+
 	}
 }
 
--- Source/Library/Renderer/Renderer.h
+++ Source/Library/Renderer/Renderer.h
@@ -1,9 +1,9 @@
 /*+===================================================================
   File:      RENDERER.H
 
-  Summary:   Renderer header file contains declarations of Renderer 
-             class used for the lab samples of Game Graphics 
-             Programming course.
+  Summary:   Renderer header file contains declarations of Renderer
+			 class used for the lab samples of Game Graphics
+			 Programming course.
 
   Classes: Renderer
 
@@ -17,43 +17,60 @@
 
 namespace library
 {
-    /*C+C+++C+++C+++C+++C+++C+++C+++C+++C+++C+++C+++C+++C+++C+++C+++C+++C
-      Class:    Renderer
+	/*C+C+++C+++C+++C+++C+++C+++C+++C+++C+++C+++C+++C+++C+++C+++C+++C+++C
+	  Struct:    SimpleVertex
 
-      Summary:  Renderer initializes Direct3D, and renders renderable 
-                data onto the screen
+	  Summary:  Simple vertex structure containing a single field of the
+				type XMFLOAT3
+	C---C---C---C---C---C---C---C---C---C---C---C---C---C---C---C---C-C*/
+	struct SimpleVertex
+	{
+		XMFLOAT3 Postion;
+	};
 
-      Methods:  Initialize
-                  Creates Direct3D device and swap chain
-                Render
-                  Renders the frame
-                Renderer
-                  Constructor.
-                ~Renderer
-                  Destructor.
-    C---C---C---C---C---C---C---C---C---C---C---C---C---C---C---C---C-C*/
-    class Renderer final
-    {
-    public:
-        Renderer();
-        Renderer(const Renderer& other) = delete;
-        Renderer(Renderer&& other) = delete;
-        Renderer& operator=(const Renderer& other) = delete;
-        Renderer& operator=(Renderer&& other) = delete;
-        ~Renderer() = default;
+	/*C+C+++C+++C+++C+++C+++C+++C+++C+++C+++C+++C+++C+++C+++C+++C+++C+++C
+	  Class:    Renderer
 
-        HRESULT Initialize(_In_ HWND hWnd);
-        void Render();
+	  Summary:  Renderer initializes Direct3D, and renders renderable
+				data onto the screen
 
-    private:
-        D3D_DRIVER_TYPE m_driverType;
-        D3D_FEATURE_LEVEL m_featureLevel;
-        ComPtr<ID3D11Device> m_d3dDevice;
-        ComPtr<ID3D11Device1> m_d3dDevice1;
-        ComPtr<ID3D11DeviceContext> m_immediateContext;
-        ComPtr<ID3D11DeviceContext1> m_immediateContext1;
-        ComPtr<IDXGISwapChain> m_swapChain;
-        ComPtr<IDXGISwapChain1> m_swapChain1;
-        ComPtr<ID3D11RenderTargetView> m_renderTargetView;
-    };
+	  Methods:  Initialize
+				  Creates Direct3D device and swap chain
+				Render
+				  Renders the frame
+				Renderer
+				  Constructor.
+				~Renderer
+				  Destructor.
+	C---C---C---C---C---C---C---C---C---C---C---C---C---C---C---C---C-C*/
+	class Renderer final
+	{
+	public:
+		Renderer();
+		Renderer(const Renderer& other) = delete;
+		Renderer(Renderer&& other) = delete;
+		Renderer& operator=(const Renderer& other) = delete;
+		Renderer& operator=(Renderer&& other) = delete;
+		~Renderer() = default;
+
+		HRESULT Initialize(_In_ HWND hWnd);
+		void Render();
+
+	private:
+		HRESULT compileShaderFromFile(_In_ PCWSTR pszFileName, _In_ PCSTR pszEntryPoint, _In_ PCSTR szShaderModel, _Outptr_ ID3DBlob** ppBlobOut);
+
+		D3D_DRIVER_TYPE m_driverType;
+		D3D_FEATURE_LEVEL m_featureLevel;
+		ComPtr<ID3D11Device> m_d3dDevice;
+		ComPtr<ID3D11Device1> m_d3dDevice1;
+		ComPtr<ID3D11DeviceContext> m_immediateContext;
+		ComPtr<ID3D11DeviceContext1> m_immediateContext1;
+		ComPtr<IDXGISwapChain> m_swapChain;
+		ComPtr<IDXGISwapChain1> m_swapChain1;
+		ComPtr<ID3D11RenderTargetView> m_renderTargetView;
+		ComPtr<ID3D11VertexShader> m_vertexShader;
+		ComPtr<ID3D11PixelShader> m_pixelShader;
+		ComPtr<ID3D11InputLayout> m_vertexLayout;
+		ComPtr<ID3D11Buffer> m_vertexBuffer;
+	};
 }
