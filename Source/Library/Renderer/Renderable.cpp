3586d6df3a2ff0b50f84ba9acb6f972cd2cdba0a
Eunseop Shim <lumeriith@gmail.com>
2022-04-08 20:30:13 +0900

    [LAB04] Implement Renderable getter and setter methods

--- Source/Library/Renderer/Renderable.cpp
+++ Source/Library/Renderer/Renderable.cpp
@@ -2,137 +2,147 @@
 
 namespace library
 {
-    /*M+M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M
-      Method:   Renderable::initialize
+	/*M+M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M
+	  Method:   Renderable::initialize
 
-      Summary:  Initializes the buffers and the world matrix
+	  Summary:  Initializes the buffers and the world matrix
 
-      Args:     ID3D11Device* pDevice
-                  The Direct3D device to create the buffers
-                ID3D11DeviceContext* pImmediateContext
-                  The Direct3D context to set buffers
+	  Args:     ID3D11Device* pDevice
+				  The Direct3D device to create the buffers
+				ID3D11DeviceContext* pImmediateContext
+				  The Direct3D context to set buffers
 
-      Modifies: [m_vertexBuffer, m_indexBuffer, m_constantBuffer, 
-                  m_world].
+	  Modifies: [m_vertexBuffer, m_indexBuffer, m_constantBuffer,
+				  m_world].
 
-      Returns:  HRESULT
-                  Status code
-    M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M-M*/
-    /*--------------------------------------------------------------------
-      TODO: Renderable::initialize definition (remove the comment)
-    --------------------------------------------------------------------*/
+	  Returns:  HRESULT
+				  Status code
+	M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M-M*/
+	HRESULT Renderable::initialize(_In_ ID3D11Device* pDevice, _In_ ID3D11DeviceContext* pImmediateContext)
+	{
+		return E_NOTIMPL;
+	}
 
-    /*M+M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M
-      Method:   Renderable::SetVertexShader
+	/*M+M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M
+	  Method:   Renderable::SetVertexShader
 
-      Summary:  Sets the vertex shader to be used for this renderable 
-                object
+	  Summary:  Sets the vertex shader to be used for this renderable
+				object
 
-      Args:     const std::shared_ptr<VertexShader>& vertexShader
-                  Vertex shader to set to
+	  Args:     const std::shared_ptr<VertexShader>& vertexShader
+				  Vertex shader to set to
 
-      Modifies: [m_vertexShader].
-    M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M-M*/
-    /*--------------------------------------------------------------------
-      TODO: Renderable::SetVertexShader definition (remove the comment)
-    --------------------------------------------------------------------*/
+	  Modifies: [m_vertexShader].
+	M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M-M*/
+	void Renderable::SetVertexShader(_In_ const std::shared_ptr<VertexShader>& vertexShader)
+	{
+		m_vertexShader = vertexShader;
+	}
 
-    /*M+M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M
-      Method:   Renderable::SetPixelShader
+	/*M+M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M
+	  Method:   Renderable::SetPixelShader
 
-      Summary:  Sets the pixel shader to be used for this renderable
-                object
+	  Summary:  Sets the pixel shader to be used for this renderable
+				object
 
-      Args:     const std::shared_ptr<PixelShader>& pixelShader
-                  Pixel shader to set to
+	  Args:     const std::shared_ptr<PixelShader>& pixelShader
+				  Pixel shader to set to
 
-      Modifies: [m_pixelShader].
-    M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M-M*/
-    /*--------------------------------------------------------------------
-      TODO: Renderable::SetPixelShader definition (remove the comment)
-    --------------------------------------------------------------------*/
+	  Modifies: [m_pixelShader].
+	M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M-M*/
+	void Renderable::SetPixelShader(_In_ const std::shared_ptr<PixelShader>& pixelShader)
+	{
+		m_pixelShader = pixelShader;
+	}
 
-    /*M+M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M
-      Method:   Renderable::GetVertexShader
+	/*M+M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M
+	  Method:   Renderable::GetVertexShader
 
-      Summary:  Returns the vertex shader
+	  Summary:  Returns the vertex shader
 
-      Returns:  ComPtr<ID3D11VertexShader>&
-                  Vertex shader. Could be a nullptr
-    M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M-M*/
-    /*--------------------------------------------------------------------
-      TODO: Renderable::GetVertexShader definition (remove the comment)
-    --------------------------------------------------------------------*/
+	  Returns:  ComPtr<ID3D11VertexShader>&
+				  Vertex shader. Could be a nullptr
+	M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M-M*/
+	ComPtr<ID3D11VertexShader>& Renderable::GetVertexShader()
+	{
+		return m_vertexShader->GetVertexShader();
+	}
 
-    /*M+M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M
-      Method:   Renderable::GetPixelShader
+	/*M+M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M
+	  Method:   Renderable::GetPixelShader
 
-      Summary:  Returns the vertex shader
+	  Summary:  Returns the vertex shader
 
-      Returns:  ComPtr<ID3D11PixelShader>&
-                  Pixel shader. Could be a nullptr
-    M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M-M*/
-    /*--------------------------------------------------------------------
-      TODO: Renderable::GetPixelShader definition (remove the comment)
-    --------------------------------------------------------------------*/
+	  Returns:  ComPtr<ID3D11PixelShader>&
+				  Pixel shader. Could be a nullptr
+	M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M-M*/
+	ComPtr<ID3D11PixelShader>& Renderable::GetPixelShader()
+	{
+		return m_pixelShader->GetPixelShader();
+	}
 
-    /*M+M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M
-      Method:   Renderable::GetVertexLayout
+	/*M+M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M
+	  Method:   Renderable::GetVertexLayout
 
-      Summary:  Returns the vertex input layout
+	  Summary:  Returns the vertex input layout
 
-      Returns:  ComPtr<ID3D11InputLayout>&
-                  Vertex input layout
-    M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M-M*/
-    /*--------------------------------------------------------------------
-      TODO: Renderable::GetVertexLayout definition (remove the comment)
-    --------------------------------------------------------------------*/
+	  Returns:  ComPtr<ID3D11InputLayout>&
+				  Vertex input layout
+	M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M-M*/
+	ComPtr<ID3D11InputLayout>& Renderable::GetVertexLayout()
+	{
+		return m_vertexShader->GetVertexLayout(); // TODO is this right
+	}
 
-    /*M+M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M
-      Method:   Renderable::GetVertexBuffer
+	/*M+M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M
+	  Method:   Renderable::GetVertexBuffer
 
-      Summary:  Returns the vertex buffer
+	  Summary:  Returns the vertex buffer
 
-      Returns:  ComPtr<ID3D11Buffer>&
-                  Vertex buffer
-    M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M-M*/
-    /*--------------------------------------------------------------------
-      TODO: Renderable::GetVertexBuffer definition (remove the comment)
-    --------------------------------------------------------------------*/
+	  Returns:  ComPtr<ID3D11Buffer>&
+				  Vertex buffer
+	M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M-M*/
+	ComPtr<ID3D11Buffer>& Renderable::GetVertexBuffer()
+	{
+		return m_vertexBuffer;
+	}
 
-    /*M+M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M
-      Method:   Renderable::GetIndexBuffer
+	/*M+M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M
+	  Method:   Renderable::GetIndexBuffer
 
-      Summary:  Returns the index buffer
+	  Summary:  Returns the index buffer
 
-      Returns:  ComPtr<ID3D11Buffer>&
-                  Index buffer
-    M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M-M*/
-    /*--------------------------------------------------------------------
-      TODO: Renderable::GetIndexBuffer definition (remove the comment)
-    --------------------------------------------------------------------*/
+	  Returns:  ComPtr<ID3D11Buffer>&
+				  Index buffer
+	M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M-M*/
+	ComPtr<ID3D11Buffer>& Renderable::GetIndexBuffer()
+	{
+		return m_indexBuffer;
+	}
 
-    /*M+M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M
-      Method:   Renderable::GetConstantBuffer
+	/*M+M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M
+	  Method:   Renderable::GetConstantBuffer
 
-      Summary:  Returns the constant buffer
+	  Summary:  Returns the constant buffer
 
-      Returns:  ComPtr<ID3D11Buffer>&
-                  Constant buffer
-    M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M-M*/
-    /*--------------------------------------------------------------------
-      TODO: Renderable::GetConstantBuffer definition (remove the comment)
-    --------------------------------------------------------------------*/
+	  Returns:  ComPtr<ID3D11Buffer>&
+				  Constant buffer
+	M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M-M*/
+	ComPtr<ID3D11Buffer>& Renderable::GetConstantBuffer()
+	{
+		return m_constantBuffer;
+	}
 
-    /*M+M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M
-      Method:   Renderable::GetWorldMatrix
+	/*M+M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M
+		  Method:   Renderable::GetWorldMatrix
 
-      Summary:  Returns the world matrix
+		  Summary:  Returns the world matrix
 
-      Returns:  const XMMATRIX&
-                  World matrix
-    M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M-M*/
-    /*--------------------------------------------------------------------
-      TODO: Renderable::GetWorldMatrix definition (remove the comment)
-    --------------------------------------------------------------------*/
+		  Returns:  const XMMATRIX&
+					  World matrix
+	M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M-M*/
+	const XMMATRIX& Renderable::GetWorldMatrix() const
+	{
+		return m_world;
+	}
 }
