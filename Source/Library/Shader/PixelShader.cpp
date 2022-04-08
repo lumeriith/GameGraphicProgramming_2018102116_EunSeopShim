e910c9bf3a70188e1b57a87ef58cc38ef3d0eae1
Eunseop Shim <lumeriith@gmail.com>
2022-04-08 19:43:31 +0900

    [LAB04] Implement shader classes

--- Source/Library/Shader/PixelShader.cpp
+++ Source/Library/Shader/PixelShader.cpp
@@ -2,50 +2,63 @@
 
 namespace library
 {
-    /*M+M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M
-      Method:   PixelShader::PixelShader
+	/*M+M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M
+	  Method:   PixelShader::PixelShader
 
-      Summary:  Constructor
+	  Summary:  Constructor
 
-      Args:     PCWSTR pszFileName
-                  Name of the file that contains the shader code
-                PCSTR pszEntryPoint
-                  Name of the shader entry point functino where shader
-                  execution begins
-                PCSTR pszShaderModel
-                  Specifies the shader target or set of shader features
-                  to compile against
+	  Args:     PCWSTR pszFileName
+				  Name of the file that contains the shader code
+				PCSTR pszEntryPoint
+				  Name of the shader entry point functino where shader
+				  execution begins
+				PCSTR pszShaderModel
+				  Specifies the shader target or set of shader features
+				  to compile against
 
-      Modifies: [m_pixelShader].
-    M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M-M*/
-    /*--------------------------------------------------------------------
-      TODO: PixelShader::PixelShader definition (remove the comment)
-    --------------------------------------------------------------------*/
+	  Modifies: [m_pixelShader].
+	M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M-M*/
+	PixelShader::PixelShader(_In_ PCWSTR pszFileName, _In_ PCSTR pszEntryPoint, _In_ PCSTR pszShaderModel) :
+		Shader(pszFileName, pszEntryPoint, pszShaderModel),
+		m_pixelShader()
+	{}
 
-    /*M+M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M
-      Method:   PixelShader::Initialize
+	/*M+M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M
+	  Method:   PixelShader::Initialize
 
-      Summary:  Initializes the pixel shader
+	  Summary:  Initializes the pixel shader
 
-      Args:     ID3D11Device* pDevice
-                  The Direct3D device to create the pixel shader
+	  Args:     ID3D11Device* pDevice
+				  The Direct3D device to create the pixel shader
 
-      Returns:  HRESULT
-                  Status code
-    M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M-M*/
-    /*--------------------------------------------------------------------
-      TODO: PixelShader::Initialize definition (remove the comment)
-    --------------------------------------------------------------------*/
+	  Returns:  HRESULT
+				  Status code
+	M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M-M*/
+	HRESULT PixelShader::Initialize(_In_ ID3D11Device* pDevice)
+	{
+		HRESULT hr;
 
-    /*M+M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M
-      Method:   PixelShader::GetPixelShader
+		// Compile and Create Pixel Shader
+		ComPtr<ID3DBlob> psBlob = nullptr;
+		hr = compile(&psBlob);
+		if (FAILED(hr)) return hr;
 
-      Summary:  Returns the pixel shader
+		hr = pDevice->CreatePixelShader(psBlob->GetBufferPointer(), psBlob->GetBufferSize(), nullptr, &m_pixelShader);
+		if (FAILED(hr)) return hr;
 
-      Returns:  ComPtr<ID3D11PixelShader>&
-                  Pixel shader. Could be a nullptr
-    M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M-M*/
-    /*--------------------------------------------------------------------
-      TODO: PixelShader::GetPixelShader definition (remove the comment)
-    --------------------------------------------------------------------*/
+		return S_OK;
+	}
+
+	/*M+M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M
+	  Method:   PixelShader::GetPixelShader
+
+	  Summary:  Returns the pixel shader
+
+	  Returns:  ComPtr<ID3D11PixelShader>&
+				  Pixel shader. Could be a nullptr
+	M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M-M*/
+	ComPtr<ID3D11PixelShader>& PixelShader::GetPixelShader()
+	{
+		return m_pixelShader;
+	}
 }
--- Source/Library/Shader/VertexShader.cpp
+++ Source/Library/Shader/VertexShader.cpp
@@ -2,62 +2,87 @@
 
 namespace library
 {
-    /*M+M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M
-      Method:   VertexShader::VertexShader
+	/*M+M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M
+	  Method:   VertexShader::VertexShader
 
-      Summary:  Constructor
+	  Summary:  Constructor
 
-      Args:     PCWSTR pszFileName
-                  Name of the file that contains the shader code
-                PCSTR pszEntryPoint
-                  Name of the shader entry point functino where shader
-                  execution begins
-                PCSTR pszShaderModel
-                  Specifies the shader target or set of shader features
-                  to compile against
+	  Args:     PCWSTR pszFileName
+				  Name of the file that contains the shader code
+				PCSTR pszEntryPoint
+				  Name of the shader entry point functino where shader
+				  execution begins
+				PCSTR pszShaderModel
+				  Specifies the shader target or set of shader features
+				  to compile against
 
-      Modifies: [m_vertexShader].
-    M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M-M*/
-    /*--------------------------------------------------------------------
-      TODO: VertexShader::VertexShader definition (remove the comment)
-    --------------------------------------------------------------------*/
+	  Modifies: [m_vertexShader].
+	M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M-M*/
+	VertexShader::VertexShader(_In_ PCWSTR pszFileName, _In_ PCSTR pszEntryPoint, _In_ PCSTR pszShaderModel) :
+		Shader(pszFileName, pszEntryPoint, pszShaderModel),
+		m_vertexShader(),
+		m_vertexLayout()
+	{}
 
-    /*M+M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M
-      Method:   VertexShader::Initialize
+	/*M+M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M
+	  Method:   VertexShader::Initialize
 
-      Summary:  Initializes the vertex shader and the input layout
+	  Summary:  Initializes the vertex shader and the input layout
 
-      Args:     ID3D11Device* pDevice
-                  The Direct3D device to create the vertex shader
+	  Args:     ID3D11Device* pDevice
+				  The Direct3D device to create the vertex shader
 
-      Returns:  HRESULT
-                  Status code
-    M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M-M*/
-    /*--------------------------------------------------------------------
-      TODO: VertexShader::Initialize definition (remove the comment)
-    --------------------------------------------------------------------*/
+	  Returns:  HRESULT
+				  Status code
+	M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M-M*/
+	HRESULT VertexShader::Initialize(_In_ ID3D11Device* pDevice)
+	{
+		HRESULT hr;
 
-    /*M+M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M
-      Method:   VertexShader::GetVertexShader
+		// Compile and Create Vertex Shader
+		ComPtr<ID3DBlob> vsBlob = nullptr;
+		hr = compile(&vsBlob);
+		if (FAILED(hr)) return hr;
 
-      Summary:  Returns the vertex shader
+		hr = pDevice->CreateVertexShader(vsBlob->GetBufferPointer(), vsBlob->GetBufferSize(), NULL, &m_vertexShader);
+		if (FAILED(hr)) return hr;
 
-      Returns:  ComPtr<ID3D11VertexShader>&
-                  Vertex shader. Could be a nullptr
-    M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M-M*/
-    /*--------------------------------------------------------------------
-      TODO: VertexShader::GetVertexShader definition (remove the comment)
-    --------------------------------------------------------------------*/
+		// Define and Create Input Layout
+		D3D11_INPUT_ELEMENT_DESC layout[] =
+		{
+			{ "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0 },
+		};
+		UINT numElements = ARRAYSIZE(layout);
 
-    /*M+M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M
-      Method:   VertexShader::GetVertexLayout
+		hr = pDevice->CreateInputLayout(layout, numElements, vsBlob->GetBufferPointer(), vsBlob->GetBufferSize(), &m_vertexLayout);
+		if (FAILED(hr)) return hr;
 
-      Summary:  Returns the vertex input layout
+		return S_OK;
+	}
 
-      Returns:  ComPtr<ID3D11InputLayout>&
-                  Vertex input layout
-    M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M-M*/
-    /*--------------------------------------------------------------------
-      TODO: VertexShader::GetVertexLayout definition (remove the comment)
-    --------------------------------------------------------------------*/
+	/*M+M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M
+	  Method:   VertexShader::GetVertexShader
+
+	  Summary:  Returns the vertex shader
+
+	  Returns:  ComPtr<ID3D11VertexShader>&
+				  Vertex shader. Could be a nullptr
+	M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M-M*/
+	ComPtr<ID3D11VertexShader>& VertexShader::GetVertexShader()
+	{
+		return m_vertexShader;
+	}
+
+	/*M+M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M
+	  Method:   VertexShader::GetVertexLayout
+
+	  Summary:  Returns the vertex input layout
+
+	  Returns:  ComPtr<ID3D11InputLayout>&
+				  Vertex input layout
+	M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M-M*/
+	ComPtr<ID3D11InputLayout>& VertexShader::GetVertexLayout()
+	{
+		return m_vertexLayout;
+	}
 }
