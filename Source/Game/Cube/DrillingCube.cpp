b609499b22d4a0b7512688550c397dcfbc9bc897
Eunseop Shim <lumeriith@gmail.com>
2022-04-10 14:14:37 +0900

    [LAB04] Make drilling cube move downwards

--- Source/Game/Cube/DrillingCube.cpp
+++ Source/Game/Cube/DrillingCube.cpp
@@ -5,10 +5,7 @@ HRESULT DrillingCube::Initialize(_In_ ID3D11Device* pDevice, _In_ ID3D11DeviceCo
 	HRESULT hr = BaseCube::Initialize(pDevice, pImmediateContext);
 	if (FAILED(hr)) return hr;
 
-	Scale(0.3f, 2.0f, 0.3f);
-
-	XMFLOAT3 trVector(0.0f, 2.0f, 0.0f);
-	Translate(XMLoadFloat3(&trVector));
+	Scale(0.3f, 8.0f, 0.3f);
 
 	return S_OK;
 }
@@ -17,4 +14,5 @@ void DrillingCube::Update(_In_ FLOAT deltaTime)
 {
 	// 2,000RPM Yuna Kim Cube
 	RotateY(deltaTime * 16.0f);
+	Translate(XMVectorSet(0.0f, -1.0f * deltaTime, 0.0f, 0.0f));
 }
