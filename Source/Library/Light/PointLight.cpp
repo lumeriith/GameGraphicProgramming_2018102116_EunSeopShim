2746ff368c4e5e3e732efa700754f2173862161e
Eunseop Shim <lumeriith@gmail.com>
2022-04-21 23:42:07 +0900

    [LAB06] Implement light classes

--- Source/Library/Light/PointLight.cpp
+++ Source/Library/Light/PointLight.cpp
@@ -4,56 +4,59 @@
 
 namespace library
 {
-    /*M+M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M
-      Method:   PointLight::PointLight
+	/*M+M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M
+	  Method:   PointLight::PointLight
 
-      Summary:  Constructor
+	  Summary:  Constructor
 
-      Args:     const XMFLOAT4& position
-                  Position of the light
-                const XMFLOAT4& color
-                  Position of the color
+	  Args:     const XMFLOAT4& position
+				  Position of the light
+				const XMFLOAT4& color
+				  Position of the color
 
-      Modifies: [m_position, m_color].
-    M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M-M*/
-    /*--------------------------------------------------------------------
-      TODO: PointLight::PointLight definition (remove the comment)
-    --------------------------------------------------------------------*/
+	  Modifies: [m_position, m_color].
+	M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M-M*/
+	PointLight::PointLight(_In_ const XMFLOAT4& position, _In_ const XMFLOAT4& color) :
+		m_position(position),
+		m_color(color)
+	{ }
 
-    /*M+M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M
-      Method:   PointLight::GetPosition
+	/*M+M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M
+	  Method:   PointLight::GetPosition
 
-      Summary:  Returns the position of the light
+	  Summary:  Returns the position of the light
 
-      Returns:  const XMFLOAT4&
-                  Position of the light
-    M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M-M*/
-    /*--------------------------------------------------------------------
-      TODO: PointLight::GetPosition definition (remove the comment)
-    --------------------------------------------------------------------*/
+	  Returns:  const XMFLOAT4&
+				  Position of the light
+	M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M-M*/
+	const XMFLOAT4& PointLight::GetPosition() const
+	{
+		return m_position;
+	}
 
-    /*M+M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M
-      Method:   PointLight::GetColor
+	/*M+M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M
+	  Method:   PointLight::GetColor
 
-      Summary:  Returns the color of the light
+	  Summary:  Returns the color of the light
 
-      Returns:  const XMFLOAT4&
-                  Color of the light
-    M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M-M*/
-    /*--------------------------------------------------------------------
-      TODO: PointLight::GetColor definition (remove the comment)
-    --------------------------------------------------------------------*/
+	  Returns:  const XMFLOAT4&
+				  Color of the light
+	M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M-M*/
+	const XMFLOAT4& PointLight::GetColor() const
+	{
+		return m_color;
+	}
 
-    /*M+M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M
-      Method:   PointLight::Update
+	/*M+M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M
+	  Method:   PointLight::Update
 
-      Summary:  Updates the light every frame
+	  Summary:  Updates the light every frame
 
-      Args:     FLOAT deltaTime
-                  Elapsed time
-    M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M-M*/
-    void PointLight::Update(_In_ FLOAT deltaTime)
-    {
-        UNREFERENCED_PARAMETER(deltaTime);
-    }
+	  Args:     FLOAT deltaTime
+				  Elapsed time
+	M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M-M*/
+	void PointLight::Update(_In_ FLOAT deltaTime)
+	{
+		UNREFERENCED_PARAMETER(deltaTime);
+	}
 }
