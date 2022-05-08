4aed4f92781d7f961337f385c16506e7eec812b2
Eunseop Shim <lumeriith@gmail.com>
2022-05-08 19:03:17 +0900

    [LAB07] Load shaders and models

--- Source/Game/Main.cpp
+++ Source/Game/Main.cpp
@@ -52,34 +52,67 @@ INT WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, 
 	UNREFERENCED_PARAMETER(lpCmdLine);
 
 	std::unique_ptr<library::Game> game = std::make_unique<library::Game>(L"Game Graphics Programming Lab 07: Modeling");
+	const auto& renderer = game->GetRenderer();
+	const auto& loadShader = [&](PCWSTR pszPath, PCWSTR pszName, PCSTR pszVSEntry, PCSTR pszVSModel, PCSTR pszPSEntry, PCSTR pszPSModel)
+	{
+		HRESULT hr;
 
-	// Light Cube
-	std::shared_ptr<library::VertexShader> lightVertexShader = std::make_shared<library::VertexShader>(L"Shaders/PhongShaders.fxh", "VSLightCube", "vs_5_0");
-	if (FAILED(game->GetRenderer()->AddVertexShader(L"LightShader", lightVertexShader)))
-	{
-		return 0;
-	}
+		std::shared_ptr<library::VertexShader> vs = std::make_shared<library::VertexShader>(pszPath, pszVSEntry, pszVSModel);
+		hr = renderer->AddVertexShader(pszName, vs);
+		if (FAILED(hr)) return hr;
 
-	// Light Cube
-	std::shared_ptr<library::PixelShader> lightPixelShader = std::make_shared<library::PixelShader>(L"Shaders/PhongShaders.fxh", "PSLightCube", "ps_5_0");
-	if (FAILED(game->GetRenderer()->AddPixelShader(L"LightShader", lightPixelShader)))
-	{
-		return 0;
-	}
+		std::shared_ptr<library::PixelShader> ps = std::make_shared<library::PixelShader>(pszPath, pszPSEntry, pszPSModel);
+		hr = renderer->AddPixelShader(pszName, ps);
+		if (FAILED(hr)) return hr;
 
-	std::shared_ptr<library::Model> model = std::make_shared<library::Model>("../../Content/nanosuit/nanosuit.obj");
-	if (FAILED(game->GetRenderer()->AddRenderable(L"Model", model)))
+		return S_OK;
+	};
+	const auto& loadModel = [&](PCWSTR pszPath, PCWSTR pszName, PCWSTR pszShader, float xOffset = 0)
 	{
-		return 0;
-	}
-	if (FAILED(game->GetRenderer()->SetVertexShaderOfRenderable(L"Model", L"LightShader")))
-	{
-		return 0;
-	}
-	if (FAILED(game->GetRenderer()->SetPixelShaderOfRenderable(L"Model", L"LightShader")))
-	{
-		return 0;
-	}
+		HRESULT hr;
+
+		std::shared_ptr<library::Model> model = std::make_shared<library::Model>(pszPath);
+		model->Translate(XMVectorSet(xOffset, 0.0f, 0.0f, 0.0f));
+
+		hr = renderer->AddRenderable(pszName, model);
+		if (FAILED(hr)) return hr;
+
+		hr = renderer->SetVertexShaderOfRenderable(pszName, pszShader);
+		if (FAILED(hr)) return hr;
+
+		hr = renderer->SetPixelShaderOfRenderable(pszName, pszShader);
+		if (FAILED(hr)) return hr;
+	};
+
+
+	HRESULT hr;
+
+	// Light Cube Shaders
+	hr = loadShader(
+		L"Shaders/PhongShaders.fxh", L"LightShader",
+		"VSLightCube", "vs_5_0",
+		"PSLightCube", "ps_5_0"
+	);
+	if (FAILED(hr)) return 0;
+
+	// Phong Shaders
+	hr = loadShader(
+		L"Shaders/PhongShaders.fxh", L"PhongShader",
+		"VSPhong", "vs_5_0",
+		"PSPhong", "ps_5_0"
+	);
+	if (FAILED(hr)) return 0;
+
+	hr = loadModel(L"../../Content/nanosuit/nanosuit.obj", L"NanoSuitModel", L"PhongShader");
+	if (FAILED(hr)) return 0;
+
+#ifdef SHOW_OTHER_MODELS
+	hr = loadModel(L"../../Content/backpack/backpack.obj", L"BackpackModel", L"PhongShader", 5.0f);
+	if (FAILED(hr)) return 0;
+
+	hr = loadModel(L"../../Content/cyborg/cyborg.obj", L"CyborgModel", L"PhongShader", -5.0f);
+	if (FAILED(hr)) return 0;
+#endif
 
 	XMFLOAT4 color;
 	XMStoreFloat4(&color, Colors::White);
@@ -88,22 +121,22 @@ INT WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, 
 		XMFLOAT4(-5.77f, 5.77f, -5.77f, 1.0f),
 		color
 		);
-	if (FAILED(game->GetRenderer()->AddPointLight(0, directionalLight)))
+	if (FAILED(renderer->AddPointLight(0, directionalLight)))
 	{
 		return 0;
 	}
 
 	std::shared_ptr<Cube> lightCube = std::make_shared<Cube>(color);
 	lightCube->Translate(XMVectorSet(-5.77f, 5.77f, -5.77f, 0.0f));
-	if (FAILED(game->GetRenderer()->AddRenderable(L"LightCube", lightCube)))
+	if (FAILED(renderer->AddRenderable(L"LightCube", lightCube)))
 	{
 		return 0;
 	}
-	if (FAILED(game->GetRenderer()->SetVertexShaderOfRenderable(L"LightCube", L"LightShader")))
+	if (FAILED(renderer->SetVertexShaderOfRenderable(L"LightCube", L"LightShader")))
 	{
 		return 0;
 	}
-	if (FAILED(game->GetRenderer()->SetPixelShaderOfRenderable(L"LightCube", L"LightShader")))
+	if (FAILED(renderer->SetPixelShaderOfRenderable(L"LightCube", L"LightShader")))
 	{
 		return 0;
 	}
@@ -113,22 +146,22 @@ INT WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, 
 		XMFLOAT4(0.0f, 0.0f, -5.0f, 1.0f),
 		color
 		);
-	if (FAILED(game->GetRenderer()->AddPointLight(1, rotatingDirectionalLight)))
+	if (FAILED(renderer->AddPointLight(1, rotatingDirectionalLight)))
 	{
 		return 0;
 	}
 
 	std::shared_ptr<RotatingCube> rotatingLightCube = std::make_shared<RotatingCube>(color);
 	rotatingLightCube->Translate(XMVectorSet(0.0f, 0.0f, -1.0f, 0.0f));
-	if (FAILED(game->GetRenderer()->AddRenderable(L"RotatingLightCube", rotatingLightCube)))
+	if (FAILED(renderer->AddRenderable(L"RotatingLightCube", rotatingLightCube)))
 	{
 		return 0;
 	}
-	if (FAILED(game->GetRenderer()->SetVertexShaderOfRenderable(L"RotatingLightCube", L"LightShader")))
+	if (FAILED(renderer->SetVertexShaderOfRenderable(L"RotatingLightCube", L"LightShader")))
 	{
 		return 0;
 	}
-	if (FAILED(game->GetRenderer()->SetPixelShaderOfRenderable(L"RotatingLightCube", L"LightShader")))
+	if (FAILED(renderer->SetPixelShaderOfRenderable(L"RotatingLightCube", L"LightShader")))
 	{
 		return 0;
 	}
