93ec408d89fdc306859253f51609a9eb7f326962
Eunseop Shim <lumeriith@gmail.com>
2022-05-15 15:59:04 +0900

    [ASS02] Fix faulty byte offset of input layout

--- Source/Library/Shader/VertexShader.cpp
+++ Source/Library/Shader/VertexShader.cpp
@@ -91,7 +91,7 @@ namespace library
 				1,
 				DXGI_FORMAT_R32G32B32A32_FLOAT,
 				1,
-				0,
+				D3D11_APPEND_ALIGNED_ELEMENT,
 				D3D11_INPUT_PER_INSTANCE_DATA,
 				1
 			},
@@ -100,7 +100,7 @@ namespace library
 				2,
 				DXGI_FORMAT_R32G32B32A32_FLOAT,
 				1,
-				0,
+				D3D11_APPEND_ALIGNED_ELEMENT,
 				D3D11_INPUT_PER_INSTANCE_DATA,
 				1
 			},
@@ -109,7 +109,7 @@ namespace library
 				3,
 				DXGI_FORMAT_R32G32B32A32_FLOAT,
 				1,
-				0,
+				D3D11_APPEND_ALIGNED_ELEMENT,
 				D3D11_INPUT_PER_INSTANCE_DATA,
 				1
 			},
